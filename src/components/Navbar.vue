251ff60bcf4e1a2c878a2830ca65e91f43e328f9
jolicam <johalicamantor@gmail.com>
2024-10-26 12:36:26 -0500

    subir todos los nuevos cambios

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,20 @@
 <template>
   <el-menu
     :default-active="activeIndex"
-      background-color="#a5a7a5 "
-      text-color="#fff"
-    class="el-menu-demo"
+    background-color="#a5a7a5"  
+    text-color="#fff" 
     mode="horizontal"
     :ellipsis="false"
     @select="handleSelect"
   >
     <el-menu-item index="0">
-      <img
-        style="width: 100px"
-       
-        alt="Element logo"
-      />
+      <span class="brand">CYCOMPANY S.A.S</span> 
     </el-menu-item>
 
-    <el-sub-menu index="1" >
+    <el-sub-menu index="1">
       <template #title>Johanna Camacho</template>
       <el-menu-item index="2-1">Perfil</el-menu-item>
-      <el-menu-item index="2-2">Cerrar Sesion</el-menu-item>
-
+      <el-menu-item index="2-2">Cerrar Sesión</el-menu-item>
     </el-sub-menu>
   </el-menu>
 </template>
@@ -35,7 +29,7 @@ const handleSelect = (key: string, keyPath: string[]) => {
 </script>
 
 <style scoped>
-.el-menu-demo{
+.el-menu-demo {
   top: 0;
   width: 100%;
   z-index: 10;
@@ -43,8 +37,13 @@ const handleSelect = (key: string, keyPath: string[]) => {
 
 .el-menu-item, 
 span.el-avatar.el-avatar--circle {
-margin-right: auto;
-padding: 0;
+  margin-right: auto;
+  padding: 0;
 }
 
+.brand {
+  font-size: 24px; /* Tamaño de la fuente para el texto */
+  font-weight: bold; /* Negrita */
+  color: white; /* Color del texto */
+}
 </style>
--- src/components/SideBarMenu.vue
+++ src/components/SideBarMenu.vue
@@ -8,19 +8,34 @@
         text-color="#fff"
         collapse-transition
       >
+        <!-- Administrador -->
         <el-sub-menu index="1">
           <template #title>
-            <span>Gestión</span>
+            <span>Administrador</span>
           </template>
-
           <el-menu-item index="1-1">Especialidades</el-menu-item>
           <el-menu-item index="1-2">Clientes</el-menu-item>
           <el-menu-item index="1-3">Vehículos</el-menu-item>
           <el-menu-item index="1-4">Mecánicos</el-menu-item>
           <el-menu-item index="1-5">Servicios</el-menu-item>
           <el-menu-item index="1-6">Repuestos</el-menu-item>
-          <el-menu-item index="1-7">Servicios Mecanicos</el-menu-item>
+          <el-menu-item index="1-7">Servicios Mecánicos</el-menu-item>
           <el-menu-item index="1-8">Facturas</el-menu-item>
+        </el-sub-menu>
+
+        <!-- Configuración -->
+        <el-sub-menu index="2">
+          <template #title>
+            <span>Configuración</span>
+          </template>
+          <el-menu-item index="2-1">Especialidades</el-menu-item>
+          <el-menu-item index="2-2">Clientes</el-menu-item>
+          <el-menu-item index="2-3">Vehículos</el-menu-item>
+          <el-menu-item index="2-4">Mecánicos</el-menu-item>
+          <el-menu-item index="2-5">Servicios</el-menu-item>
+          <el-menu-item index="2-6">Repuestos</el-menu-item>
+          <el-menu-item index="2-7">Servicios Mecánicos</el-menu-item>
+          <el-menu-item index="2-8">Facturas</el-menu-item>
         </el-sub-menu>
       </el-menu>
     </el-col>
