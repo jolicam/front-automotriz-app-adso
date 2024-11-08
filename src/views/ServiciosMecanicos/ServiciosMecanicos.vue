aa9250e856d5148375fd06e905dd9759e755fe3b
jolicam <johalicamantor@gmail.com>
2024-11-08 18:48:53 -0500

    nuevos cambios modificados

--- src/views/ServiciosMecanicos/ServiciosMecanicos.vue
+++ src/views/ServiciosMecanicos/ServiciosMecanicos.vue
@@ -1,195 +1,116 @@
 <template>
-    <LayoutMain>
-      <Header />
-  
-      <div class="container">
-        <h1>Servicios Mecánicos</h1>
-  
-        <!-- Botón para abrir el formulario -->
-        <el-button type="primary" @click="abrirFormulario">Agregar Servicio</el-button>
-  
-        <!-- Tabla de servicios mecánicos -->
-        <el-table :data="serviciosMecanicos" style="width: 100%">
-          <el-table-column prop="servicio_id" label="Servicio ID" />
-          <el-table-column prop="mecanico_id" label="Mecánico ID" />
-          <el-table-column prop="repuesto_id" label="Repuesto ID" />
-  
-          <el-table-column label="Acciones">
-            <template #default="scope">
-              <el-button
-                @click="editarFormulario(scope.row)"
-                type="warning"
-                icon="el-icon-edit"
-                size="small"
-              >
-                Editar
-              </el-button>
-              <el-button
-                @click="eliminarServicio(scope.row)"
-                type="danger"
-                icon="el-icon-delete"
-                size="small"
-              >
-                Eliminar
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-  
-        <!-- Formulario para agregar/editar servicios -->
-        <el-dialog :visible.sync="mostrarFormulario" title="Formulario Servicio Mecánico">
-          <Formulario
-            v-if="mostrarFormulario"
-            :editando="editandoFormulario"
-            :servicios="servicios"
-            :mecanicos="mecanicos"
-            :repuestos="repuestos"
-            :formRef="formRef"
-            @guardar="guardarDatos"
-          />
-        </el-dialog>
-      </div>
-    </LayoutMain>
-  </template>
-  
-  <script lang="ts" setup>
-  import { onMounted, reactive, ref } from 'vue'
-  import LayoutMain from '../../components/LayoutMain.vue';
-  import Formulario from '../../components/Formulario.vue';
-  import Header from '../../components/Header.vue';
-  import { Delete, Edit } from "@element-plus/icons-vue";
-  import { ElMessage } from 'element-plus'
-  import axios from 'axios';
-  
-  const mostrarFormulario = ref(false)
-  const editandoFormulario = ref(false)
-  const formRef = ref()
-  const servicios = ref([]) 
-  const mecanicos = ref([]) 
-  const serviciosMecanicos = ref([]) 
-  
-  const abrirFormulario = () => {
-    mostrarFormulario.value = true
-    editandoFormulario.value = false
+  <LayoutMain>
+    <template #slotLayout>
+      <Header :titulo="'Servicios Mecánicos'" :tituloBoton="'Crear Servicio Mecánico'" :abrir="abrirFormulario" />
+
+      <Formulario :titulo="'Gestión de Servicios Mecánicos'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" @save="guardarDatos">
+        <template #slotForm>
+          <el-row :gutter="20">
+            <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+              <formServiciosMecanicos v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" ref="formRef" />
+            </el-col>
+          </el-row>
+        </template>
+      </Formulario>
+
+      <el-table :data="serviciosMecanicos" stripe style="width: 100%">
+        <el-table-column prop="servicio_id" label="ID Servicio" />
+        <el-table-column prop="mecanico_id" label="ID Mecánico" />
+        <el-table-column prop="repuesto_id" label="ID Repuesto" />
+        <el-table-column fixed="right" label="Acciones" min-width="120">
+          <template #default>
+            <el-button link type="primary" size="large" :icon="Edit" @click="editarFormulario"></el-button>
+            <el-button link type="danger" :icon="Delete" @click="eliminarServicioMecanico"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+  </LayoutMain>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import LayoutMain from '../../components/LayoutMain.vue'
+import Formulario from '../../components/Formulario.vue'
+import Header from '../../components/Header.vue'
+import { Delete, Edit } from "@element-plus/icons-vue"
+import formServiciosMecanicos from "./components/formServiciosMecanicos.vue" // Verifica que este nombre esté correcto
+import { ElMessage } from 'element-plus'
+import axios from 'axios'
+
+const mostrarFormulario = ref(false)
+const editandoFormulario = ref(false)
+const formRef = ref()
+const serviciosMecanicos = ref([])
+
+const abrirFormulario = () => {
+  mostrarFormulario.value = true
+  editandoFormulario.value = false
+}
+
+const editarFormulario = async () => {
+  mostrarFormulario.value = true
+  editandoFormulario.value = true
+}
+
+const guardarDatos = async () => {
+  const validacion = await formRef.value?.validarFormulario()
+  if (validacion) {
+    await crearServicioMecanico()
   }
-  
-  const editarFormulario = async (row) => {
-    mostrarFormulario.value = true
-    editandoFormulario.value = true
-    // Aquí puedes agregar código para llenar el formulario con los datos de 'row' (la fila seleccionada)
+}
+
+const crearServicioMecanico = async () => {
+  const url = 'http://127.0.0.1:8000/api/serviciomecanico/save'
+
+  const dataFormulario = {
+    servicio_id: formRef.value.formulario.servicio_id,
+    mecanico_id: formRef.value.formulario.mecanico_id,
+    repuesto_id: formRef.value.formulario.repuesto_id
   }
-  
-  const guardarDatos = async () => {
-    const validacion = await formRef.value?.validarFormulario()
-    if (validacion) {
-        await crearServicioMecanico()
-    }
+
+  try {
+    await axios.post(url, dataFormulario)
+      .then((response) => {
+        console.log(response)
+        formRef.value?.limpiarFormulario()
+        ElMessage({
+          message: 'El servicio mecánico se creó con éxito.',
+          type: 'success',
+        })
+        cargarServiciosMecanicos()
+        mostrarFormulario.value = false
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error al crear servicio mecánico', error)
   }
-  
-  const crearServicioMecanico = async () => {
-    const url = 'http://127.0.0.1:8000/api/serviciomecanico/save'
-  
-    const dataFormulario = {
-        servicio_id: formRef.value.formulario.servicio_id,
-        mecanico_id: formRef.value.formulario.mecanico_id,
-        repuesto_id: formRef.value.formulario.repuesto_id,
-    }
-  
-    try {
-        axios.post(url, dataFormulario)
-            .then(function (response) {
-                console.log(response);
-                formRef.value?.limpiarFormulario()
-                ElMessage({
-                    message: 'El servicio mecánico se creó con éxito.',
-                    type: 'success',
-                })
-                datosServicioMecanico()
-                mostrarFormulario.value = false
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-  
-    } catch (error) {
-        console.error('Error al crear servicio mecánico ', error)
-    }
+}
+
+const eliminarServicioMecanico = async () => {
+  console.log('Servicio mecánico eliminado')
+}
+
+const cargarServiciosMecanicos = async () => {
+  const url = 'http://127.0.0.1:8000/api/serviciomecanico/datos'
+
+  try {
+    await axios.get(url)
+      .then((response) => {
+        serviciosMecanicos.value = response.data.result
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error al cargar datos de servicios mecánicos', error)
   }
-  
-  const datosServicioMecanico = async () => {
-    const url = 'http://127.0.0.1:8000/api/serviciomecanico/datos'
-  
-    try {
-        axios.get(url)
-            .then(function (response) {
-                serviciosMecanicos.value = response.data.result
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-  
-    } catch (error) {
-        console.error('Error al obtener datos de servicios mecánicos ', error)
-    }
-  }
-  
-  const getServicios = async () => {
-    const url = 'http://127.0.0.1:8000/api/servicio/datos'
-  
-    try {
-        axios.get(url)
-            .then(function (response) {
-                servicios.value = response.data.result
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    } catch (error) {
-        console.error('Error al obtener servicios ', error)
-    }
-  }
-  
-  const getMecanicos = async () => {
-    const url = 'http://127.0.0.1:8000/api/mecanicos/datos'
-  
-    try {
-        axios.get(url)
-            .then(function (response) {
-                mecanicos.value = response.data.result
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    } catch (error) {
-        console.error('Error al obtener mecánicos ', error)
-    }
-  }
-  
-  const getRepuestos = async () => {
-    const url = 'http://127.0.0.1:8000/api/repuestos/datos'
-  
-    try {
-        axios.get(url)
-            .then(function (response) {
-                repuestos.value = response.data.result
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    } catch (error) {
-        console.error('Error al obtener repuestos ', error)
-    }
-  }
-  
-  onMounted(() => {
-    getServicios()
-    getMecanicos()
-    getRepuestos()
-    datosServicioMecanico()
-  })
-  </script>
-  
+}
+
+onMounted(() => {
+  cargarServiciosMecanicos()
+})
+</script>
--- src/views/Vehiculos/Vehiculos.vue
+++ src/views/Vehiculos/Vehiculos.vue
@@ -1,215 +1,120 @@
 <template>
-    <LayoutMain>
-      <template #slotLayout>
-        <!-- Encabezado -->
-        <Header :titulo="'Vehículos'" :tituloBoton="'Crear Vehículo'" :abrir="abrirFormulario" />
-  
-        <!-- Formulario de Vehículo -->
-        <Formulario :titulo="'Gestión de Vehículos'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario"
-          @save="guardarDatos">
-          <template #slotForm>
-            <el-row :gutter="20">
-              <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-                <FormVehiculo v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" ref="formRef"
-                  :marcas="marcas" :tipos="tipos" />
-              </el-col>
-            </el-row>
+  <LayoutMain>
+    <template #slotLayout>
+      <Header :titulo="'Vehículos'" :tituloBoton="'Crear Vehículo'" :abrir="abrirFormulario" />
+
+      <Formulario :titulo="'Gestión de Vehículos'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" @save="guardarDatos">
+        <template #slotForm>
+          <el-row :gutter="20">
+            <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+              <formVehiculos v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" ref="formRef" />
+            </el-col>
+          </el-row>
+        </template>
+      </Formulario>
+
+      <el-table :data="vehiculos" stripe style="width: 100%">
+        <el-table-column prop="placa" label="Placa" />
+        <el-table-column prop="marca" label="Marca" />
+        <el-table-column prop="modelo" label="Modelo" />
+        <el-table-column prop="año" label="Año" />
+        <el-table-column prop="cliente_id" label="ID Cliente" />
+        <el-table-column fixed="right" label="Acciones" min-width="120">
+          <template #default>
+            <el-button link type="primary" size="large" :icon="Edit" @click="editarFormulario"></el-button>
+            <el-button link type="danger" :icon="Delete" @click="eliminarVehiculo"></el-button>
           </template>
-        </Formulario>
-  
-        <!-- Tabla de Vehículos -->
-        <el-table :data="vehiculos" stripe style="width: 100%">
-          <el-table-column prop="placa" label="Placa" />
-          <el-table-column prop="marca" label="Marca" />
-          <el-table-column prop="tipo" label="Tipo" />
-          <el-table-column prop="anio" label="Año" />
-          <el-table-column fixed="right" label="Acciones" min-width="120">
-            <template #default>
-              <el-button link type="primary" size="large" :icon="Edit" @click="editarFormulario"></el-button>
-              <el-button link type="danger" :icon="Delete" @click="eliminarVehiculo"></el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-  
-      </template>
-    </LayoutMain>
-  </template>
-  
-  <script lang="ts" setup>
-  import { onMounted, ref } from 'vue'
-  import LayoutMain from '../../components/LayoutMain.vue';
-  import Formulario from '../../components/Formulario.vue';
-  import Header from '../../components/Header.vue';
-  import { Delete, Edit } from "@element-plus/icons-vue"
-  import { ElMessage } from 'element-plus'
-  import formVehiculos from '../../components/formVehiculos.vue';
+        </el-table-column>
+      </el-table>
+    </template>
+  </LayoutMain>
+</template>
 
-  
-  // Variables de control
-  const mostrarFormulario = ref(false)
-  const editandoFormulario = ref(false)
-  const formRef = ref()
-  const marcas = ref([])
-  const tipos = ref([])
-  const vehiculos = ref([])
-  
-  
-  const abrirFormulario = () => {
-    mostrarFormulario.value = true
-    editandoFormulario.value = false
-  }
-  
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import LayoutMain from '../../components/LayoutMain.vue'
+import Formulario from '../../components/Formulario.vue'
+import Header from '../../components/Header.vue'
+import { Delete, Edit } from "@element-plus/icons-vue"
+import formVehiculos from "./components/formVehiculos.vue" // Asegúrate que este nombre es correcto
+import { ElMessage } from 'element-plus'
+import axios from 'axios'
 
-  const editarFormulario = async () => {
-    mostrarFormulario.value = true
-    editandoFormulario.value = true
+const mostrarFormulario = ref(false)
+const editandoFormulario = ref(false)
+const formRef = ref()
+const vehiculos = ref([])
+
+const abrirFormulario = () => {
+  mostrarFormulario.value = true
+  editandoFormulario.value = false
+}
+
+const editarFormulario = async () => {
+  mostrarFormulario.value = true
+  editandoFormulario.value = true
+}
+
+const guardarDatos = async () => {
+  const validacion = await formRef.value?.validarFormulario()
+  if (validacion) {
+    await crearVehiculo()
   }
-  
-  
-  const guardarDatos = async () => {
-    const validacion = await formRef.value?.validarFormulario()
-    if (validacion) {
-      if (editandoFormulario.value) {
-        await actualizarVehiculo()
-      } else {
-        await crearVehiculo()
-      }
-    }
+}
+
+const crearVehiculo = async () => {
+  const url = 'http://127.0.0.1:8000/api/vehiculo/save'
+
+  const dataFormulario = {
+    placa: formRef.value.formulario.placa,
+    marca: formRef.value.formulario.marca,
+    modelo: formRef.value.formulario.modelo,
+    año: formRef.value.formulario.año,
+    cliente_id: formRef.value.formulario.cliente_id
   }
-  
-  // Función para crear un vehículo
-  const crearVehiculo = async () => {
-    const url = 'http://127.0.0.1:8000/api/vehiculos/save'
-    const dataFormulario = {
-      placa: formRef.value.formulario.placa,
-      marca: formRef.value.formulario.marca,
-      tipo: formRef.value.formulario.tipo,
-      anio: formRef.value.formulario.anio,
-    }
-  
-    try {
-      axios.post(url, dataFormulario)
-        .then(function (response) {
-          ElMessage({
-            message: 'El vehículo se creó con éxito.',
-            type: 'success',
-          })
-          formRef.value?.limpiarFormulario()
-          datosVehiculo()
-          mostrarFormulario.value = false
+
+  try {
+    await axios.post(url, dataFormulario)
+      .then((response) => {
+        console.log(response)
+        formRef.value?.limpiarFormulario()
+        ElMessage({
+          message: 'El vehículo se creó con éxito.',
+          type: 'success',
         })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-    } catch (error) {
-      console.error('Error al crear vehículo', error)
-    }
+        cargarVehiculos()
+        mostrarFormulario.value = false
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error al crear vehículo', error)
   }
-  
-  // Función para actualizar un vehículo
-  const actualizarVehiculo = async () => {
-    const url = 'http://127.0.0.1:8000/api/vehiculos/update'
-    const dataFormulario = {
-      id: formRef.value.formulario.id,
-      placa: formRef.value.formulario.placa,
-      marca: formRef.value.formulario.marca,
-      tipo: formRef.value.formulario.tipo,
-      anio: formRef.value.formulario.anio,
-    }
-  
-    try {
-      axios.put(url, dataFormulario)
-        .then(function (response) {
-          ElMessage({
-            message: 'El vehículo se actualizó con éxito.',
-            type: 'success',
-          })
-          datosVehiculo()
-          mostrarFormulario.value = false
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-    } catch (error) {
-      console.error('Error al actualizar vehículo', error)
-    }
+}
+
+const eliminarVehiculo = async () => {
+  console.log('Vehículo eliminado')
+}
+
+const cargarVehiculos = async () => {
+  const url = 'http://127.0.0.1:8000/api/vehiculo/datos'
+
+  try {
+    await axios.get(url)
+      .then((response) => {
+        vehiculos.value = response.data.result
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error al cargar datos de vehículos', error)
   }
-  
-  // Función para eliminar un vehículo
-  const eliminarVehiculo = async (id: string) => {
-    const url = `http://127.0.0.1:8000/api/vehiculos/delete/${id}`
-  
-    try {
-      axios.delete(url)
-        .then(function (response) {
-          ElMessage({
-            message: 'El vehículo se eliminó con éxito.',
-            type: 'success',
-          })
-          datosVehiculo()
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-    } catch (error) {
-      console.error('Error al eliminar vehículo', error)
-    }
-  }
-  
-  // Función para obtener los datos de los vehículos
-  const datosVehiculo = async () => {
-    const url = 'http://127.0.0.1:8000/api/vehiculos/datos'
-  
-    try {
-      axios.get(url)
-        .then(function (response) {
-          vehiculos.value = response.data.result
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-    } catch (error) {
-      console.error('Error al obtener los vehículos', error)
-    }
-  }
-  
- 
-  const getMarcasYTipos = async () => {
-    const urlMarcas = 'http://127.0.0.1:8000/api/marcas/datos'
-    const urlTipos = 'http://127.0.0.1:8000/api/tipos/datos'
-  
-    try {
-      axios.get(urlMarcas)
-        .then(function (response) {
-          marcas.value = response.data.result
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-      axios.get(urlTipos)
-        .then(function (response) {
-          tipos.value = response.data.result
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  
-    } catch (error) {
-      console.error('Error al obtener marcas o tipos', error)
-    }
-  }
-  
- 
-  onMounted(() => {
-    getMarcasYTipos()
-    datosVehiculo()
-  })
-  
-  </script>
-  
-  
+}
+
+onMounted(() => {
+  cargarVehiculos()
+})
+</script>
