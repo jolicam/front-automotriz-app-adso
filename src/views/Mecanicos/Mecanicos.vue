18170182e7d5a3d87ade73864e0c3580bf0eb6e7
jolicam <johalicamantor@gmail.com>
2024-12-02 18:20:06 -0500

    mmmm

--- src/views/Mecanicos/Mecanicos.vue
+++ src/views/Mecanicos/Mecanicos.vue
@@ -39,7 +39,7 @@ import { onMounted, reactive, ref } from 'vue'
 import LayoutMain from '../../components/LayoutMain.vue'
 import Formulario from '../../components/Formulario.vue'
 import Header from '../../components/Header.vue'
-import formMecanicos from './components/formMecanicos.vue'
+import formMecanicos from './Components/formMecanicos.vue'
 import { Delete, Edit } from "@element-plus/icons-vue"
 import { ElMessage, ElMessageBox } from 'element-plus'
 import axios from 'axios'
